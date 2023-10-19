1210728196b99a1eda0b9a38edbc3d710e0166bd
Simontagbor <simontagbor360@gmail.com>
2023-10-19 12:09:58 -0700

    fix the counting of filesize to maintain a running total of file size

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-"""
-This Module parses a stdin stream of logs
-"""
 import sys
 import re
 from collections import Counter, OrderedDict
+"""Parses log files"""
+
+
+file_sizes = []
+allowed_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
 
 def parse_line(lines):
-    """Parse each line and process data """
     status_codes = []
-    file_sizes = []
     pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(' \
               r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] ' \
               r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
     count = 0
     for line in lines:
-        if count <= 10:
-            match = re.match(pattern, line)
-            if match:
-                status_codes.append(match.group(3))
-                file_sizes.append(int(match.group(4)))
-            count += 1
-        else:
+        match = re.match(pattern, line)
+        if match:
+            status_code = int(match.group(3))
+            if status_code in allowed_codes:
+                status_codes.append(status_code)
+            file_sizes.append(int(match.group(4)))
+        count += 1
+        if count >= 10:
             break
     sorted_codes = sorted(status_codes)
     status_occurrences = dict(OrderedDict(Counter(sorted_codes)))
@@ -30,7 +31,7 @@ def parse_line(lines):
 
 
 def print_stat(stats):
-    print(f"File size: {stats[0]}")
+    print(f"Filesize: {stats[0]}")
     for key, value in stats[1].items():
         print(f"{key}: {value}")
 
@@ -38,7 +39,7 @@ def print_stat(stats):
 def main():
     lines = sys.stdin
     try:
-        while lines is not None:
+        while True:
             for _ in range(10):
                 stats = parse_line(lines)
                 print_stat(stats)
@@ -46,4 +47,5 @@ def main():
         pass
 
 
-main()
+if __name__ == "__main__":
+    main()
