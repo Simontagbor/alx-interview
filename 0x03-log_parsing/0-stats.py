f974b180d8a1f240becaff3d2602fca30866fd2a
Simontagbor <simontagbor360@gmail.com>
2023-10-20 02:51:13 -0700

    add module documentation

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,19 +1,37 @@
 #!/usr/bin/python3
+"""
+Log Parsing Module
+
+This module provides functions for parsing
+log files and generating statistics from log data.
+"""
 import sys
 import re
 from collections import Counter, OrderedDict
-"""Parses log files"""
-
 
 file_sizes = []
 allowed_codes = [200, 301, 400, 401, 403, 404, 405, 500]
 
 
 def parse_line(lines):
+    """Parse log lines and return statistics.
+
+    Args:
+        lines (iterable): An iterable containing log lines.
+
+    Returns:
+        tuple: A tuple containing the total file size
+        and a dictionary of status code counts.
+
+    This function parses log lines, filters out
+    invalid status codes, and counts the occurrences
+    of valid status codes within sets of 10 lines.
+
+    """
     status_codes = []
-    pattern = r'^(\d+\.\d+\.\d+\.\d+) - \[(' \
-              r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] ' \
-              r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$'
+    pattern = (r'^(\d+\.\d+\.\d+\.\d+) - \[('
+               r'\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\.\d+)\] '
+               r'"GET /projects/\d+ HTTP/1\.1" (\d+) (\d+)$')
     count = 0
     for line in lines:
         match = re.match(pattern, line)
@@ -31,12 +49,24 @@ def parse_line(lines):
 
 
 def print_stat(stats):
+    """Print statistics.
+
+    Args:
+        stats (list): A list containing the total file
+        size and a dictionary of status code counts.
+
+    Returns:
+        None
+    This function prints the total file size and a
+    count of status codes.
+    """
     print(f"Filesize: {stats[0]}")
     for key, value in stats[1].items():
         print(f"{key}: {value}")
 
 
 def main():
+    """Main function."""
     lines = sys.stdin
     try:
         while True:
