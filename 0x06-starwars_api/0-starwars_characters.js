b5e3a0d1c653b15b5fc78fe9cb99529ec1002082
Simontagbor <simontagbor360@gmail.com>
2023-11-09 00:06:59 -0800

    let script return ordered output of characters

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -3,41 +3,65 @@ const request = require('request');
 const movieId = process.argv[2];
 
 if (!movieId) {
-  console.log('Usage: node script.js <Movie ID>');
+  console.log('Usage: ./script.js <Movie ID>');
   process.exit(1);
 }
 
 const baseUrl = 'https://swapi.dev/api/films/';
 
-request.get(`${baseUrl}${movieId}/`, (error, response, movieData) => {
-  if (error) {
-    console.error(`Error fetching movie data: ${error}`);
-    return;
-  }
-
-  if (response.statusCode !== 200) {
-    console.error(`Error fetching movie data. Status Code: ${response.statusCode}`);
-    return;
-  }
-
-  const movie = JSON.parse(movieData);
-  const characterLinks = movie.characters;
-
-  characterLinks.forEach((characterLink) => {
-    request.get(characterLink, (characterError, characterResponse, characterData) => {
-      if (characterError) {
-        console.error(`Error fetching characters: ${characterError}`);
-        return;
-      }
-
-      if (characterResponse.statusCode !== 200) {
-        console.error(`Error fetching characters. Status Code: ${characterResponse.statusCode}`);
-        return;
-      }
-
-      const character = JSON.parse(characterData);
-      console.log(character.name);
-    });
+const fetchMovieData = (movieId, callback) => {
+  const url = `${baseUrl}${movieId}/`;
+  request(url, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+    if (response.statusCode !== 200) {
+      return callback(new Error(`Error fetching movie data. Status Code: ${response.statusCode}`));
+    }
+    const movieData = JSON.parse(body);
+    callback(null, movieData);
   });
-});
+};
+
+const fetchCharacterData = (characterUrl, callback) => {
+  request(characterUrl, (error, response, body) => {
+    if (error) {
+      return callback(error);
+    }
+    if (response.statusCode !== 200) {
+      return callback(new Error(`Error fetching character data. Status Code: ${response.statusCode}`));
+    }
+    const characterData = JSON.parse(body);
+    callback(null, characterData);
+  });
+};
+
+const printCharacterNames = (movieId) => {
+  fetchMovieData(movieId, (movieError, movieData) => {
+    if (movieError) {
+      return console.error(movieError.message);
+    }
+    const characterUrls = movieData.characters;
+    let currentIndex = 0;
+
+    const printNextCharacterName = () => {
+      if (currentIndex >= characterUrls.length) {
+        return; 
+      }
+
+      fetchCharacterData(characterUrls[currentIndex], (characterError, characterData) => {
+        if (characterError) {
+          return console.error(characterError.message);
+        }
+        console.log(characterData.name);
+        currentIndex++;
+        printNextCharacterName(); 
+      });
+    };
+
+    printNextCharacterName(); 
+  });
+};
+
+printCharacterNames(movieId);
 
