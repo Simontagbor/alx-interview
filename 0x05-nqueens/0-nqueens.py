2f96f8662e01836bb4b907efe14961a4e2c39b64
Simontagbor <simontagbor360@gmail.com>
2024-01-12 16:00:09 +0000

    add 0-nqueens puzzle solution

---
+++ 0x05-nqueens/0-nqueens.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""N Queens puzzle
+The N queens puzzle is the challenge of
+placing N non-attacking queens on an
+N×N chessboard.
+"""
+import sys
+
+# set usage
+if len(sys.argv) != 2:
+    print("Usage: nqueens N")
+    sys.exit(1)
+elif not sys.argv[1].isdigit():
+    print("N must be a number")
+    sys.exit(1)
+elif int(sys.argv[1]) < 4:
+    print("N must be at least 4")
+    sys.exit(1)
+else:
+    N = int(sys.argv[1])
+    # print every possible solution
+    for i in range(1, N-1):
+        solution = []
+        for j in range(0, N):
+            solution.append([j, i])
+        print(solution)
