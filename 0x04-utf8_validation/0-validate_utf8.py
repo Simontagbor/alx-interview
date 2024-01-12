ac4b8fbf248b03c0c0f13926c4dfda0b1a198427
Simontagbor <simontagbor360@gmail.com>
2024-01-12 11:06:43 +0000

    add function to validate utf8

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -12,25 +12,19 @@ def validUTF8(data):
     Returns:
             bool - true if data is utf-8 encoded.
     """
-    bytes_left = 0
-
-    for byte in data:
-        byte_bin = format(byte, '08b')
-
-        if bytes_left == 0:
-            if byte_bin.startswith('0'):
-                bytes_left = 0
-            elif byte_bin.startswith('110'):
-                bytes_left = 1
-            elif byte_bin.startswith('1110'):
-                bytes_left = 2
-            elif byte_bin.startswith('11110'):
-                bytes_left = 3
-            else:
+    n_bytes = 0
+    for num in data:
+        bin_rep = format(num, '#010b')[-8:]
+        if n_bytes == 0:
+            for bit in bin_rep:
+                if bit == '0': break
+                n_bytes += 1
+            if n_bytes == 0:
+                continue
+            if n_bytes == 1 or n_bytes > 4:
                 return False
         else:
-            if not byte_bin.startswith('10'):
+            if not (bin_rep[0] == '1' and bin_rep[1] == '0'):
                 return False
-            bytes_left -= 1
-
-    return bytes_left == 0
+        n_bytes -= 1
+    return n_bytes == 0
